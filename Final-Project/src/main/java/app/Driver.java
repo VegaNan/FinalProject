82f273fc8ef8d1c418b918bc4841968d215ed6a2
VegaNan <47089431+VegaNan@users.noreply.github.com>
2019-02-27 15:39:42 -0700

    Added run

--- Final-Project/src/main/java/app/Driver.java
+++ Final-Project/src/main/java/app/Driver.java
@@ -3,7 +3,7 @@ package app;
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		GameMaster.run();
 
 	}
 
--- Final-Project/src/main/java/app/GameMaster.java
+++ Final-Project/src/main/java/app/GameMaster.java
@@ -1,5 +1,7 @@
 package app;
 
 public class GameMaster {
-
+	public static void run(){
+		
+	}
 }
