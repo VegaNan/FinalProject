594b4fd7b9316e96f65b68394d152ebc4f132e1f
VegaNan <47089431+VegaNan@users.noreply.github.com>
2019-02-27 17:13:39 -0700

    Made GUI pretty

--- Final-Project/src/main/java/gui/Start.java
+++ Final-Project/src/main/java/gui/Start.java
@@ -11,56 +11,58 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-
 public class Start extends Application {
-	
-	
+		
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Label Start = new Label();
-			VBox root = new VBox();
-			root.setAlignment(Pos.CENTER);	
-			Start.setMinSize(2000, 200);
-			
-			Button StartButton = new Button("Start");
-			Button startNew = new Button("Start New Game");
-			Button loadGame = new Button("Load Game");
-			Button quit = new Button("Quit");
-			StartButton.setOnAction(new EventHandler<ActionEvent>() {
-				@Override
-				public void handle(ActionEvent event) {
-					VBox menuOptions = new VBox();
-
-					Scene scene = new Scene(menuOptions, 1600, 800);
-					menuOptions.setAlignment(Pos.CENTER);
-					menuOptions.setPadding(new Insets(20, 80, 20, 80));
-					menuOptions.getChildren().add(startNew);
-					menuOptions.getChildren().add(loadGame);
-					menuOptions.getChildren().add(quit);
-					
-					primaryStage.setScene(scene);
-					primaryStage.setTitle("Main Menu");
-					primaryStage.show();
-				}
-			});
-			
-			VBox switchBox = new VBox();
-			switchBox.setAlignment(Pos.CENTER);
-			switchBox.setPadding(new Insets(20, 80, 20, 80));
-			switchBox.getChildren().add(StartButton);
-			
-			root.getChildren().addAll(Start, switchBox);
-			
-			Scene scene = new Scene(root, 1600, 800);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			primaryStage.setScene(startMenu(primaryStage));
 			primaryStage.setTitle("Main Menu");
 			primaryStage.show();
 			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public Scene startMenu(Stage primaryStage) {
+		Label Start = new Label();
+		VBox root = new VBox();
+		root.setAlignment(Pos.CENTER);	
+		Start.setMinSize(200, 200);
+		Button StartButton = new Button("Start");
+		StartButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				mainMenu(primaryStage);
+			}
+		});
+		
+		VBox switchBox = new VBox();
+		switchBox.setAlignment(Pos.CENTER);
+		switchBox.setPadding(new Insets(20, 80, 20, 80));
+		switchBox.getChildren().add(StartButton);
+		root.getChildren().addAll(Start, switchBox);
+		Scene scene = new Scene(root, 1600, 800);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		return scene;
+	}
+	
+	public void mainMenu(Stage primaryStage) {
+		Button startNew = new Button("Start New Game");
+		Button loadGame = new Button("Load Game");
+		Button quit = new Button("Quit");
+		VBox menuOptions = new VBox();
+		Scene scene = new Scene(menuOptions, 1600, 800);
+		menuOptions.setAlignment(Pos.CENTER);
+		menuOptions.setPadding(new Insets(20, 80, 20, 80));
+		menuOptions.getChildren().add(startNew);
+		menuOptions.getChildren().add(loadGame);
+		menuOptions.getChildren().add(quit);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Main Menu");
+		primaryStage.show();
 	}
 	
 	
