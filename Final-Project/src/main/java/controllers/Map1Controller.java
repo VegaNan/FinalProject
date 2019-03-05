9189a3a5f8438a4233199ff8e5e66c6a10cdb6cc
Order66exe <zwslocum@yahoo.com>
2019-03-05 15:32:39 -0700

    Map1 controller fixes

--- Final-Project/src/main/java/controllers/Map1Controller.java
+++ Final-Project/src/main/java/controllers/Map1Controller.java
@@ -1,7 +1,11 @@
 package controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,11 +15,11 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import models.Player;
 
-public class Map1Controller {
+public class Map1Controller implements Initializable{
 	@FXML private Player player1 = new Player(300,300,100,100, Color.BLUE, 1,1,1,1);
 	@FXML private AnchorPane ap = new AnchorPane();
 	Scene scene = ap.getScene();
-	scene.
+	
 	scene.setOnKeyPressed(new EventHandler<KeyEvent>())
 	{
 		@Override
@@ -34,4 +38,9 @@ public class Map1Controller {
 			}
 		}
 	}
+		@Override
+		public void initialize(URL arg0, ResourceBundle arg1) {
+			// TODO Auto-generated method stub
+			
+		}
 }
