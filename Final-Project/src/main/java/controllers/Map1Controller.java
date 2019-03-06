55739499d36933fbbfc349cfab86b07ae18844f1
VegaNan <47089431+VegaNan@users.noreply.github.com>
2019-03-06 15:35:42 -0700

    Fixed Map1Controller

--- Final-Project/src/main/java/controllers/Map1Controller.java
+++ Final-Project/src/main/java/controllers/Map1Controller.java
@@ -1,51 +1,15 @@
 package controllers;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.paint.Color;
-import models.Player;
-
-public class Map1Controller implements Initializable {
-
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		Player player1 = new Player(300,300,100,100,Color.BLUE, 1,1,1,1);
-		Parent parent = null;
-		try {
-			parent = FXMLLoader.load(getClass().getResource("/view/Map1.fxml"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		Scene scene = parent.getScene();
-		parent.getChildren().add(player1);
-		scene.setOnKeyPressed(key -> {
-		
-		});
-	}
-
-}
-=======
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
-import javafx.stage.Stage;
 import models.Player;
 
 public class Map1Controller implements Initializable {
