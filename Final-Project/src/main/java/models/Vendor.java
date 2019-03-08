40fd443e8368723bd8d63ee4dc99258d9240fb70
Ja'Mar McCoy <jamccoy@student.neumont.edu>
2019-03-07 17:04:39 -0700

    Started on vendor and added CombatView Class

--- Final-Project/src/main/java/models/Vendor.java
+++ Final-Project/src/main/java/models/Vendor.java
@@ -2,6 +2,34 @@ package models;
 
 import java.util.ArrayList;
 
+import utilities.RNG;
+
 public class Vendor {
 	protected ArrayList<Item> inventory = new ArrayList<>();
+
+	public Vendor() {
+
+	}
+
+	// Selling items to the vendor
+	public int sellingItems(Item item) {
+		int goldAmount = 0;
+		int itemValue = item.getValue();
+
+		// Vendor will buy items for 90% of the items original price
+		if (itemValue >= 10) {
+			goldAmount = itemValue - (itemValue / 10);
+		} else {
+			int randValue = RNG.generateInt(1, 7);
+			goldAmount = itemValue - randValue;
+
+		}
+		return goldAmount;
+	}
+	// Buying items from the vendor
+	public int buyingItems() {
+		return 0;
+		
+	}
+
 }
